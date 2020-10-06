3fa1d07ef38bdf12b8a3abcbd57ea7294e2588a4
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-06 19:27:15 +0200

    wip

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -8,6 +8,9 @@ export default function FriendsList(props) {
   return (
     <div className='list-friends container'>
       {/* 3- We need to loop over the data rendering a Friend as we go */}
+      {friends.map((friendObj) => {
+
+      })}
       {/* Each friend is going to need a `key` prop and also some other prop with data */}
     </div>
   )
