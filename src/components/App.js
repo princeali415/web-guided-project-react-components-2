28ccf91935f2ac9a7319524dfcebb9a13814323d
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-06 18:30:45 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import FriendsList from './FriendsList'
 import Search from './Search'
 // 2- Import the dummy data that will power the application.
 // (Tomorrow we'll fetch the data from an API instead.)
-import friends from '../dummy-data/friends'
+import friends, { hello, goodbye } from '../dummy-data/friends'
 
 export default function App() {
   // 3- Initialize a slice of state to keep track of the data
