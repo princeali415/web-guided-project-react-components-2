656a23ac4447889e150ce5662ceac10369c861a9
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-06 19:02:04 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -24,11 +24,12 @@ export default function App() {
   function changeStatus(id) {
     setMyFriends(myFriends.map(friend => {
       // for each friend, if the friend has the given id,
-      if (friend.id === id) {
-        // return a copy of the friend object which has the toggled civil status
-        return { ...friend,  married: !}
-      }
-      // otherwse return the friend unchanged
+      // if (friend.id === id) {
+      //   // return a copy of the friend object which has the toggled civil status
+      //   return { ...friend,  married: !friend.married }
+      // }
+      // // otherwse return the friend unchanged
+      // return friend
     }))
   }
 
